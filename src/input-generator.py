5fccbab1ca3fbe224e07fa1fa0d5854d069f96b7
Will Berg <willberg510@gmail.com>
2026-01-30 15:32:52 -0500

    Add comments to input generator file

--- src/input-generator.py
+++ src/input-generator.py
@@ -1,12 +1,15 @@
 import random
 
+'''
+This file can be run to write a valid input file for generated.in given some input n.
+'''
 n = int(input("Enter n: "))
 with open("../data/generated.in", "w") as file:
   file.write(str(n))
   file.write("\n")
   numbers = list(range(1, n + 1))
   for i in range(0, 2 * n):
-    random.shuffle(numbers)
+    random.shuffle(numbers) # Creates a random permutation of 1 to n for each preference list
     for j in range(0, n):
       file.write(str(numbers[j]))
       if (j != n - 1):
